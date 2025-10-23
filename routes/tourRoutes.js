7b292e06c707c8b2e4c9b2744bfc7604c202eaff
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2025-10-23 03:20:08 +0100

    feat: post middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,12 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// check body middleware function
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
