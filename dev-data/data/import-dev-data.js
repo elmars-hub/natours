e6de75d43d96f2281425d3efe3f942a410198a7a
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2025-11-04 20:59:06 +0100

    feat: tours model

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
